cf14c92eda248b0923135239fc0db99466739c29
Speconicci <42351038+Speconicci@users.noreply.github.com>
2021-02-18 01:48:44 +0200

    Implementation of TransactionInfo class

--- CoolParking/Models/TransactionInfo.cs
+++ CoolParking/Models/TransactionInfo.cs
@@ -4,5 +4,14 @@ namespace CoolParking.Models
 {
     struct TransactionInfo
     {
+        private string transactionTime;
+        private string vehicleId;
+        private decimal vehicleBalance;
+        private decimal withdrawSum;
+
+        public string TransactionTime { get => transactionTime; }
+        public string VehicleId { get => vehicleId; }
+        public decimal VehicleBalance { get => vehicleBalance; }
+        public decimal WithdrawSum { get => withdrawSum; }
     }
 }
