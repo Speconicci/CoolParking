a87cca82be0444e11cd349fc0b5f30102c72844f
Speconicci <42351038+Speconicci@users.noreply.github.com>
2021-02-18 00:07:21 +0200

    Implementation of Parking class and Settings class

--- CoolParking/Models/Parking.cs
+++ CoolParking/Models/Parking.cs
@@ -6,5 +6,40 @@ namespace CoolParking.Models
 {
     class Parking
     {
+        public static Parking instance;
+        private decimal balance;
+        public List<Vehicle> vehicles;
+
+        private Parking()
+        {
+            this.balance = Settings.initialParkingBalance;
+            this.vehicles = new List<Vehicle>(Settings.parkingCapacity);
+        }
+
+        public decimal Balance
+        {
+            get => balance;
+        }
+
+        public void TopUp(decimal sum)
+        {
+            if (sum > 0)
+            {
+                this.balance += sum;
+            }
+        }
+
+        public static Parking GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Parking();
+                return instance;
+            }
+            else
+            {
+                return instance;
+            }
+        }
     }
 }
--- CoolParking/Models/Settings.cs
+++ CoolParking/Models/Settings.cs
@@ -1,7 +1,21 @@
-﻿
+﻿using System.Collections.Generic;
 namespace CoolParking.Models
 {
     static class Settings
     {
+        public static decimal initialParkingBalance = 0;
+        public static int parkingCapacity = 10;
+        public static float withdrawIntervalInMillis = 5000f;
+        public static float logIntervalInMillis = 12000f;
+        public static decimal penaltyMultiplier = 2.5m;
+        public static string logFilePath = "";
+
+        public static Dictionary<VehicleType, decimal> vehicleParkingTariff = new Dictionary<VehicleType, decimal>()
+        {
+            {VehicleType.PassengerCar, 2m},
+            {VehicleType.Truck, 5m},
+            {VehicleType.Bus, 3.5m},
+            {VehicleType.Motorcycle, 1m}
+        };
     }
 }
